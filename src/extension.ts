ad94531c24627990a982819616e958be2efeee7c
spicyfalafel <285574@niuitmo.ru>
2023-12-28 14:10:22 +0300

    add json<->edn<->yaml converter

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,11 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
+	// SPC m p p
 	const myProvider = new (class implements vscode.TextDocumentContentProvider {
 		provideTextDocumentContent(uri: vscode.Uri): string {
 			const tmp = uri.query.split("=")[1].substring(1);
-			// TODO: 
+			// TODO:
 			const value = tmp.substring(0, tmp.length - 1);
 			const lines = value.split("\\n");
 			return lines.join('\n');
@@ -33,6 +34,77 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+
+
+	// converter
+	//
+	const cp = require('child_process');
+	console.log("hello0");
+
+
+  let callJet = function selectedTextToJet(from:string, to:string){
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			const selection = editor.selection;
+			if (selection && !selection.isEmpty) {
+				const selectionRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
+    		const highlighted = editor.document.getText(selectionRange);
+				console.log("text " + highlighted)
+    		cp.exec(`echo '${highlighted}' | jet --to ${to} --from ${from}`, (err: Error, stdout: Buffer|String, stderr: Buffer|String) => {
+    			if (err) {
+						vscode.window.showWarningMessage('jet error: ' + err.toString());
+					} else if (stderr) {
+						vscode.window.showWarningMessage('jet error: ' + stderr.toString());
+					} else if (stdout) {
+    				console.log('stdout: ' + stdout)
+						editor.edit(editBuilder => {
+							editBuilder.replace(selectionRange, stdout.toString());
+						})
+					}
+				});
+			}
+		}
+	}
+
+	let disposable1 = vscode.commands.registerCommand('vsdoom.edn-to-json', () => {
+		callJet("edn", "json")
+	});
+
+	context.subscriptions.push(disposable1);
+
+	let disposable2 = vscode.commands.registerCommand('vsdoom.json-to-edn', () => {
+		callJet("json", "edn")
+	});
+
+	context.subscriptions.push(disposable2);
+
+
+	let disposable3 = vscode.commands.registerCommand('vsdoom.yaml-to-json', () => {
+		callJet("yaml", "json")
+	});
+
+	context.subscriptions.push(disposable3);
+
+	let disposable4 = vscode.commands.registerCommand('vsdoom.json-to-yaml', () => {
+		callJet("json", "yaml")
+	});
+
+	context.subscriptions.push(disposable4);
+
+
+	let disposable5 = vscode.commands.registerCommand('vsdoom.yaml-to-edn', () => {
+		callJet("yaml", "edn")
+	});
+
+	context.subscriptions.push(disposable5);
+
+
+	let disposable6 = vscode.commands.registerCommand('vsdoom.edn-to-yaml', () => {
+		callJet("edn", "yaml")
+	});
+
+	context.subscriptions.push(disposable6);
+
 }
 
 export function deactivate() { }
